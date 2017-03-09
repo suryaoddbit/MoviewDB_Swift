cdc6fe2c92aa62d85f7a95bf4daf97632bb45b66
I Wayan Surya Adi Yasa <surya@oddbit.id>
2017-03-09 11:01:17 +0800

    add int

--- GitLesson/ViewController.swift
+++ GitLesson/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     var str : String = "String"
+    let intvar : Int = 1
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
